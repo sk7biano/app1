e71c84c5387b637832561e063acdf83b7f019463
Your Name <you@example.com>
2022-07-30 18:21:37 -0300

    first commit

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,21 @@
+import { Frase } from '../shared/frase.model'
+
+export const FRASES: Frase[] = [
+    {fraseEng: 'I like to learn', frasePtBr:'eu gosto de aprender'},
+    {fraseEng: 'I watch tv', frasePtBr:'eu assisto tv'},
+    {fraseEng: 'How are you?', frasePtBr:'como vai você?'},
+    {fraseEng: 'I eat bread', frasePtBr:'eu como pão'}
+
+]
+
+/* export const FRASES: Frase[] = [
+    {fraseEng: 'Love', frasePtBr:'amor'},
+    {fraseEng: 'Sun and Moon', frasePtBr:'sol e lua'},
+    {fraseEng: 'Monkey', frasePtBr:'macaco'},
+    {fraseEng: 'Good Morning', frasePtBr:'bom dia'},
+    {fraseEng: 'Thank You', frasePtBr:'obrigado'},
+    {fraseEng: 'Hot dog', frasePtBr:'cachorro quente'},
+    {fraseEng: 'Bus', frasePtBr:'onibus'},
+    {fraseEng: 'Strawberry', frasePtBr:'morango'}
+
+] */
---
+++ src/app/painel/painel.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { Frase } from '../shared/frase.model';
+import { FRASES } from './frases-mock';
+
+@Component({
+  selector: 'app-painel',
+  templateUrl: './painel.component.html',
+  styleUrls: ['./painel.component.css']
+})
+export class PainelComponent implements OnInit, OnDestroy {
+
+  public frases: Frase[] = FRASES
+  public instrucao: string = 'Traduza a frase'
+  public resposta: string = ''
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
+  public progresso: number = 0
+  public tentativas: number = 3
+
+  @Output() public encerrarJogo:EventEmitter<string> = new EventEmitter()
+  constructor() {
+    this.atualizaRodada()
+  }
+
+  ngOnInit() {
+  }
+  ngOnDestroy(){
+    // console.log('Foi destruido')
+  }
+
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+    // console.log(this.resposta)
+  }
+
+  public verificarResposta(): void {
+    // console.log(this.tentativas)
+    // console.log('Verificar resposta: ', this.resposta)
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      
+      this.rodada++
+
+      if(this.rodada === 4) {
+        this.encerrarJogo.emit('vitoria')
+      }
+      //atualiza o objeto rodadaFrase
+      this.atualizaRodada()
+
+      //progresso
+      this.progresso = this.progresso + (100 / this.frases.length)
+      
+      
+    } else {
+      this.tentativas--
+      
+      if(this.tentativas === -1) {
+       this.encerrarJogo.emit('derrota')
+      }
+      // console.log(this.tentativas)
+    }
+  }
+  
+  public atualizaRodada(): void{
+    //define a frase da rodada com base em alguma lógica 
+    this.rodadaFrase = this.frases[this.rodada]
+    //limpar a resposta
+    this.resposta = ''
+  }
+}
